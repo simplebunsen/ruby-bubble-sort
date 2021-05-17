b2cefbc7ac0eab9e5f514a6e8497fce5c3677afb
simplebunsen <52760596+simplebunsen@users.noreply.github.com>
2021-05-17 21:29:52 +0200

    first roughly working version, sorts incoming array

--- sort.rb
+++ sort.rb
@@ -1,22 +1,24 @@
 def bubble_sort(array)
 
 
-  #worst case conditions
+  #calculate-no-more-than-worst-case conditions
   max_sort_iterations = array.length - 1
   is_sorted = false
 
-  #each iteration, ignore sort_i elements at the end since they will always be at the right positions
 
   for sort_i in 1..max_sort_iterations do
-    break if is_sorted #TODO: implement is_sorted setter
+    break if is_sorted #TODO: implement PROPER is_sorted setter
+    is_sorted = true #boo, hacky
 
+    #each iteration, ignore sort_i elements at the end since they will always be at the right positions
     for i in 0..array.length - 1 - sort_i do
-      array[i] < array[i+1] ? next : array[i], array[i+1] = array[i+1], array[i] #parallel assignment "swap"
+      array[i] < array[i+1] ? next : (array[i], array[i+1] = array[i+1], array[i]; is_sorted = false) #parallel assignment "swap"
     end   
 
   end
 
+  return array
 
 end
 
-puts bubble_sort([4,3,78,2,0,2])
+p bubble_sort([4,3,78,2,0,2])
