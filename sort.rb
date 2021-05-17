505edfb57e9ad01a266673bf8b86825324ae13f6
simplebunsen <52760596+simplebunsen@users.noreply.github.com>
2021-05-17 21:11:57 +0200

    written method skeleton

---
+++ sort.rb
@@ -0,0 +1,18 @@
+def bubble_sort(unsorted_array)
+
+
+  #worst case conditions
+  max_iterations = unsorted_array.length - 1
+  is_sorted = false
+
+  #each iteration, ignore i elements on the right since they will always be at the right positions
+
+  for i in 0..max_iterations do
+    
+    
+  end
+
+
+end
+
+puts bubble_sort([4,3,78,2,0,2])
